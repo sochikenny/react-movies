2d62288f4bebec831cfbcd086d88ea63d3bc157b
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-07-01 01:07:53 -0400

    Updates

---
+++ precache-manifest.20537614974b11943e4c9fabc6a59623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a12f55c0d91cf02ae39dbefd97db24",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "bcc792f804b362343374",
+    "url": "/react-movies/static/css/main.e5bf6361.chunk.css"
+  },
+  {
+    "revision": "23dc38f5ea8c95bed7e5",
+    "url": "/react-movies/static/js/2.16bd94cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movies/static/js/2.16bd94cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcc792f804b362343374",
+    "url": "/react-movies/static/js/main.e26f7a72.chunk.js"
+  },
+  {
+    "revision": "993586d17614575891f5",
+    "url": "/react-movies/static/js/runtime-main.ca0b6528.js"
+  }
+]);
