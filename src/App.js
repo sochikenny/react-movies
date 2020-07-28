4b7f01cf9b3b7a1d732a51f41fe5938c3444ec22
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-07-28 09:03:18 -0400

    switched to functional component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React from "react";
 import SearchMovies from "./SearchMovies";
 import "./styles.css";
 
-class App extends React.Component {
-    render() {
-      return (
-        <div className="container">
-        <h1 className="title">React Movie App</h1>
-        <SearchMovies />
-        </div>
-      );
-    }
-  }
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="title">React Movie App</h1>
+      <SearchMovies />
+    </div>
+  );
+};
 
-  export default App; 
+export default App;
